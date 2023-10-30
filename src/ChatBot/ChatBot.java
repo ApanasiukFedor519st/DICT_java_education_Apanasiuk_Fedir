a336f1199d49abd723bb9da2e5cb913988e8fa3a
ApanasiukFedor519st <f.m.apanasiuk@student.khai.edu>
2023-10-30 13:30:23 +0200

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -1,19 +1,26 @@
 package ChatBot;
 import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args){
-        final Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello! My name is " + "Aid" + ".");
-        System.out.println("I was created in " + "2018" + ".");
-        System.out.println("Please, remind me your name.");
-        String name = scanner.nextLine();
-        System.out.println("What a great name you have, " + name + "!");
-        System.out.println("Let me guess your age.");
-        System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
-        int r3 = scanner.nextInt();
-        int r5 = scanner.nextInt();
-        int r7 = scanner.nextInt();
-        int age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
-        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+            final Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Hello! My name is " + "Aid" + ".");
+            System.out.println("I was created in " + "2018" + ".");
+            System.out.println("Please, remind me your name.");
+            String name = scanner.nextLine();
+            System.out.println("What a great name you have, " + name + "!");
+            System.out.println("Let me guess your age.");
+            System.out.println("Say me remainders of dividing your age by 3, 5 and 7.");
+            int r3 = scanner.nextInt();
+            int r5 = scanner.nextInt();
+            int r7 = scanner.nextInt();
+            int age = (r3 * 70 + r5 * 21 + r7 * 15) % 105;
+            System.out.println("Your age is " + age + "; that's a good time to start programming!");
+            System.out.println("Now I will prove to you that I can count to any number you want.");
+            int number = scanner.nextInt();
+            for (int i = 0; i <= number; i++) {
+                System.out.printf("%d!\n", i);
+            }
+        }
     }
-}
