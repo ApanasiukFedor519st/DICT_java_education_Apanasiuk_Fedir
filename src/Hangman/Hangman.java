4b01e3d001012c6b071c810f9ea4d693735696e2
ApanasiukFedor519st <f.m.apanasiuk@student.khai.edu>
2023-12-06 10:49:38 +0200

    Hangman 6-th stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,10 +1,12 @@
 package Hangman;
 import org.apache.commons.lang3.StringUtils;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
+        ArrayList guessed = new ArrayList();
         Scanner scanner = new Scanner(System.in);
         String[] strArray = {"java", "python", "javascript", "kotlin"};
         System.out.println("HANGMAN");
@@ -17,24 +19,39 @@ public class Hangman {
         while (i < 8) {
             System.out.print("\nInput a letter:");
             String input = scanner.nextLine();
+
+            if (guessed.contains(input.charAt(0))) {
+                System.out.println("No Improvements");
+                i++;
+                continue;
+            }
+
             if (!w.contains(input)) {
                 System.out.println("That letter doesnt appear in the word!");
                 i++;
+                continue;
             }
-            else{
-                for (int z = 0; z < hidden_word.length(); z++) {
-                    if (w.charAt(z) == input.charAt(0)){
-                        hidden_word.setCharAt(z, input.charAt(0));
-                    }
-                }
-                System.out.println(hidden_word);
 
-                long lettersToGuess = StringUtils.countChar(hidden_word.toString(), '-');
-                if (lettersToGuess == 0) break;
+            guessed.add(input.charAt(0));
+
+            for (int z = 0; z < hidden_word.length(); z++) {
+                if (w.charAt(z) == input.charAt(0)){
+                    hidden_word.setCharAt(z, input.charAt(0));
+
+                }
             }
+            System.out.println(hidden_word);
+
+            long lettersToGuess = StringUtils.countChar(hidden_word.toString(), '-');
+            if (lettersToGuess == 0) break;
 
         }
-        System.out.println("You guessed the word!Ha");
+
+        if (i == 8) {
+            System.out.println("You lost!");
+            return;
+        }
+        System.out.println("You guessed the word!");
         System.out.println("You survived!");
     }
 }
